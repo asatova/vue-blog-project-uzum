998444110f15038a087948dbd267829c880ba8b4
zarnigor <zarnigorasatova52@gmail.com>
2025-06-19 18:55:08 +0500

    added files

--- blog-project-uzum/src/App.vue
+++ blog-project-uzum/src/App.vue
@@ -1,10 +1,23 @@
 <template>
-<h3></h3>
+<h3>{{ info }}</h3>
+<h3>{{ someInfo }}</h3>
+
 </template>
 
 <style scoped>
 
 </style>
 <script>
-
+export default {
+  name: 'App',
+  data() {
+    return {
+      info: 'Hello World',
+      someInfo: 'This is some info'
+    }
+  },
+  mounted() {
+    console.log('App component mounted');
+  }
+}
 </script>
